23efdca1b66cf3feb905d3dd738da8e75ee41b14
Samiul Islam <samiul2238@gmail.com>
2023-04-28 00:38:31 +0600

    The database is updated. Postgresql setting is added into to setting_rail.py file

--- text_share/settings_rail.py
+++ text_share/settings_rail.py
@@ -4,3 +4,16 @@ from decouple import config
 SECRET_KEY = config('SECRET_KEY')
 
 ALLOWED_HOSTS = ['web-production-a568.up.railway.app']
+
+DATABASES = {
+    'default': {
+        #'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('DATABASE_NAME'),
+        'USER': config('DATABASE_USER'),
+        'PASSWORD': config('DATABASE_PASSWORD'),
+        'HOST': config('DATABASE_HOST'),
+        'PORT': config('DATABASE_PORT'),
+        'OPTIONS': {'sslmode': 'require'},
+    }
+}
